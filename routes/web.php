2a6e5935a5abe1b8eefc1f8b6c560c5369008e02
egin10 <egin066@gmail.com>
2018-10-25 06:35:19 +0800

    init

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+// Grouping Route
+$router->group(['prefix' => 'admin'], function () use ($router) {
+    $router->get('dashboard', function () {
+        return "Dashboard Admin";
+    });
+    $router->get('profile', function () {
+        return "Profile Admin";
+    });
+    //use alias for route name
+    $router->get('users', ['as' => 'admin.users', function () {
+        return "Users list";
+    }]);
+    // redirect to admin.users
+    $router->get('list', function () {
+        return redirect()->route('admin.users');
+    });
+});
+
+
+
+// Generate Key for .env
+// $router->get('/key', function () {
+//     return str_random(32);
+// });
